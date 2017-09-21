e30f1ed574874fbd9a46f2daf6e0ba46e5447c08
Roper Macaraeg <5462117+Ropestar1@users.noreply.github.com>
2017-09-21 12:50:55 -0700

    finishing array method study

--- assets/js/app.js
+++ assets/js/app.js
@@ -35,7 +35,18 @@ console.log(allAdults);
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+// NOTE: below line includes an implicit return of true
+const comment = comments.find(comment => comment.id === 823423)
+console.log(comment);
+
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id === 823423);
+// console.log(index);
+// comments.slice(index, 1);
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
